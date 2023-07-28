8aeb137ce0225cafdd7d4b0f86cd96d56e8dc0dc
Dominic Webb <dwebb@reapit.com>
2023-07-28 16:46:51 +0100

    SAI now sending and receiving I2S data over loop-back

--- QuadMod/src/interrupts.h
+++ QuadMod/src/interrupts.h
@@ -1,8 +1,25 @@
-uint8_t saiTest = 0;
+uint16_t saiTest = 0;
+uint32_t saiRecBL = 0;
+uint32_t saiRecBR = 0;
+bool saiL = true;
+
 void SAI1_IRQHandler(void)
 {
-	SAI1_Block_A->DR = (uint32_t)(++saiTest);
-	SAI1_Block_A->DR = (uint32_t)(0x80000000 + saiTest);
+	// Check if interrupt is Block A FIFO request
+	if ((SAI1_Block_A->SR & SAI_xSR_FREQ) != 0) {
+		SAI1_Block_A->DR = (uint32_t)(++saiTest);
+		SAI1_Block_A->DR = (uint32_t)(0x80000000 + saiTest);
+	}
+
+	// Check if interrupt is data received in Block B
+	if ((SAI1_Block_B->SR & SAI_xSR_FREQ) != 0) {
+		if (saiL) {
+			saiRecBL = SAI1_Block_B->DR;
+		} else {
+			saiRecBR = SAI1_Block_B->DR;
+		}
+		saiL = !saiL;
+	}
 }
 
 void SysTick_Handler(void)
