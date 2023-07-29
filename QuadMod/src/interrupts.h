6eb91ea64f5acc03c44aa742ca1f20657e2f060a
Dominic Webb <dwebb@reapit.com>
2023-07-29 13:03:37 +0100

    SAI working as dual transmitter on SAI1 and dual receiver on SAI2

--- QuadMod/src/interrupts.h
+++ QuadMod/src/interrupts.h
@@ -1,6 +1,8 @@
 uint16_t saiTest = 0;
 uint32_t saiRecAL = 0;
 uint32_t saiRecAR = 0;
+uint32_t saiRecBL = 0;
+uint32_t saiRecBR = 0;
 bool saiL = true;
 struct {
 	uint32_t leftSend;
@@ -13,17 +15,18 @@ void SAI1_IRQHandler(void)
 	// Check if interrupt is Block A FIFO request
 	if ((SAI1_Block_A->SR & SAI_xSR_FREQ) != 0) {
 		SAI1_Block_A->DR = (uint32_t)(++saiTest);
-		SAI1_Block_A->DR = (uint32_t)(0x80000000 + saiTest);
+		SAI1_Block_A->DR = (uint32_t)(0x10000000 + saiTest);
+		SAI1_Block_B->DR = (uint32_t)(0x20000000 + saiTest);
+		SAI1_Block_B->DR = (uint32_t)(0x30000000 + saiTest);
 	}
 
 	// Check if interrupt is data received in SAI2 Block A
 	if ((SAI2_Block_A->SR & SAI_xSR_FREQ) != 0) {
-		//if (saiL) {
-			saiRecAL = SAI2_Block_A->DR;
-		//} else {
-			saiRecAR = SAI2_Block_A->DR;
-		//}
-		//saiL = !saiL;
+		saiRecAL = SAI2_Block_A->DR;
+		saiRecAR = SAI2_Block_A->DR;
+		saiRecBL = SAI2_Block_B->DR;
+		saiRecBR = SAI2_Block_B->DR;
+
 		debugSAI[debugCount].leftRec = saiRecAL;
 		debugSAI[debugCount++].leftSend = saiTest;
 	} else {
